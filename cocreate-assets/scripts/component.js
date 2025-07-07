0cb10fe75f650f1a4555fa0ad1bef60d36b2e54e
thispauli <pcfrieda@web.de>
2025-07-07 16:20:25 +0200

    test

---
+++ cocreate-assets/scripts/component.js
@@ -0,0 +1,16 @@
+class MyMindmap extends HTMLElement {
+  async connectedCallback() {
+    // KEIN Shadow DOM – Stil wird übernommen
+    this.innerHTML = `
+      <div id="mindmap_container">
+        <svg id="mindmap" width="1000" height="600"></svg>
+        <button id="saveButton">💾 Speichern</button>
+      </div>
+    `;
+
+    const module = await import('http://141.72.13.151:8200/script-core.js');
+    module.setupMindmap(this);  // Achtung: setupMindmap muss exportiert sein!
+  }
+}
+
+customElements.define('my_mindmap', MyMindmap);
