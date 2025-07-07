bd013f02d8597c72327e02bc4ea00c580fc78186
WeibchenL <l.molter.lm@gamil.com>
2025-07-07 13:24:07 +0200

    test wg. Bildern

--- cocreate-assets/cocreate-mindmap.js
+++ cocreate-assets/cocreate-mindmap.js
@@ -25,7 +25,7 @@ export class CoCreateMindmap extends HTMLElement {
 
       <div id="mindmap-container">
         <div id="sidebar-left" class="sidebar">
-          <img src="cocreate-assets/img/icon-manual.png" alt="Icon manual"
+          <img src="./img/icon-manual.png" alt="Icon manual"
             style="cursor: pointer;" draggable="false" 
             onclick="this.getRootNode().getElementById('dialogIconManual').showModal()">
 
@@ -36,7 +36,7 @@ export class CoCreateMindmap extends HTMLElement {
               onclick="this.closest('dialog').close()">Schließen</button>
           </dialog>
 
-          <img src="cocreate-assets/img/icon-overview.png" alt="Icon overview user"
+          <img src="./cocreate-assets/img/icon-overview.png" alt="Icon overview user"
             style="cursor: pointer;" draggable="false" 
             onclick="this.getRootNode().getElementById('dialogIconOverviewUser').showModal(); window.loadUsersForCurrentMindmap(this.getRootNode());">
 
